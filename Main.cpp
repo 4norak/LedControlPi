743e40ba10fd3feb7383b6095f22be6b6c95c5bc
4norak <anorak@localhost.localdomain>
2020-01-06 15:28:17 +0100

    Another senseless commit message

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,18 @@
 
 int main(void)
 {
-  LedControl display(10, 11, 8, 8);
-  
-  for(;;)
-      for(int i=0;i<10;i++) {
-          for(int j=0; j < 8; j++)
-              display.setCharacter(j, i);
-          delay(1000);
+    LedControl display(10, 11, 8, 8);
+    for(;;)
+        for(int i=0;i<10;i++) {
+            for(int j=0; j<8; j++)
+                display.setCharacter(j, i);
+            delay(800);
+            for(int j=0; j<8; j++)
+                display.shutdown(j, true);
+                delay(200);
+                display.shutdown(j, false);
+                display.clear(j);
+        }
 
-  return 0;
+    return 0;
 }
