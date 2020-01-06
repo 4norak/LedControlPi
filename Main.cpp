1d3ec4c4c7248a4776339578af8daacfbe82a48a
4norak <anorak@localhost.localdomain>
2020-01-06 15:02:29 +0100

    Another senseless commit message

--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,17 @@ int main(void)
 {
   LedControl display(10, 11, 8, 8);
   
-  display.setCharacter(0, '3');
-  display.setCharacter(1, '6');
-  display.setCharacter(2, '7');
-  display.setCharacter(3, '9');
-  display.setCharacter(4, '3');
-  display.setCharacter(5, '6');
-  display.setCharacter(6, '7');
-  display.setCharacter(7, '9');
-
-  delay(200);
+  for(int i=0; i<20; i++) {
+    display.setCharacter(0, '3');
+    display.setCharacter(1, '6');
+    display.setCharacter(2, '7');
+    display.setCharacter(3, '9');
+    display.setCharacter(4, '3');
+    display.setCharacter(5, '6');
+    display.setCharacter(6, '7');
+    display.setCharacter(7, '9');
+    delay(20);
+  }
 
   return 0;
 }
